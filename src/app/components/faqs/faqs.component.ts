eb641304aee5621e17aa899ebb652cff5bd40105
chiwawa123 <tauraishep@gmail.com>
2024-03-01 11:16:04 +0200

    home changes

--- src/app/components/faqs/faqs.component.ts
+++ src/app/components/faqs/faqs.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-faqs',
   templateUrl: './faqs.component.html',
-  styleUrl: './faqs.component.css'
+  styleUrls: ['./faqs.component.css']
 })
-export class FaqsComponent {
+export class FaqsComponent implements AfterViewInit {
+  @ViewChildren('accordionButton') accordionButtons!: QueryList<ElementRef<HTMLButtonElement>>;
 
+  ngAfterViewInit() {
+    this.accordionButtons.forEach(button => {
+      button.nativeElement.addEventListener('click', () => this.toggleAccordion(button.nativeElement));
+    });
+  }
+
+  toggleAccordion(button: HTMLButtonElement) {
+    const itemToggle = button.getAttribute('aria-expanded');
+
+    this.accordionButtons.forEach(button => {
+      button.nativeElement.setAttribute('aria-expanded', 'false');
+    });
+
+    if (itemToggle === 'false') {
+      button.setAttribute('aria-expanded', 'true');
+    }
+  }
 }
--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
       }, 1);
   };
   spinner();
-  
+
 
 
 
